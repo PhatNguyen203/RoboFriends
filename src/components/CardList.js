30e095d83b97dd73b06b4dca9be80d23285644a0
PhatNguyen203 <Lee0ne203@gmail.com>
2020-08-27 22:17:39 -0400

    created CardList component

---
+++ src/components/CardList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = (props) => {
+  return (
+    <div>
+      {props.robots.map((robot, i) => {
+        return (
+          <Card
+            id={props.robots[i].id}
+            name={props.robots[i].name}
+            email={props.robots[i].email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+export default CardList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import CardList from "../components/CardList";
 
 import "./App.css";
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => this.setState({ robots: data }));
+  }
+  render() {
+    return (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
 }
 
 export default App;
