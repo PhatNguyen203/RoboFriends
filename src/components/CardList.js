e27d4c26199bd14bb85ebfbf42c6aa97ce3bf2ea
PhatNguyen203 <Lee0ne203@gmail.com>
2020-08-27 22:33:15 -0400

    created SearchBox component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -7,6 +7,7 @@ const CardList = (props) => {
       {props.robots.map((robot, i) => {
         return (
           <Card
+            key={i}
             id={props.robots[i].id}
             name={props.robots[i].name}
             email={props.robots[i].email}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CardList from "../components/CardList";
-
+import SearchBox from "../components/SearchBox";
 import "./App.css";
 
 class App extends Component {
@@ -16,11 +16,21 @@ class App extends Component {
       .then((res) => res.json())
       .then((data) => this.setState({ robots: data }));
   }
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ searchField: event.target.value });
+  };
   render() {
+    const filterRobot = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
     return (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <CardList robots={this.state.robots} />
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList robots={filterRobot} />
       </div>
     );
   }
